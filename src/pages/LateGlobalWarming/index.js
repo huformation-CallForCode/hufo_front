854af4616e0b0f5fcc64b747ff05dc9945056074
Daniel <elrion018@gmail.com>
2020-06-12 17:24:11 +0900

    feature-selecting Daily, Event

--- src/pages/LateGlobalWarming/index.js
+++ src/pages/LateGlobalWarming/index.js
@@ -4,11 +4,37 @@ import { EffectCard } from "../../components";
 class LateGlobalWarming extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      daily: true,
+    };
   }
 
-  renderCard = () => {
+  handleClick = (e) => {
+    if (this.state.daily === true && e.target.innerHTML === "Event") {
+      this.setState(
+        {
+          daily: false,
+        },
+        console.log(this.state.da)
+      );
+    } else if (this.state.daily === false && e.target.innerHTML === "Daily") {
+      this.setState(
+        {
+          daily: true,
+        },
+        console.log(this.state.daily)
+      );
+    }
+  };
+
+  renderDailyCard = () => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return arr.map(({ index }) => {
+      return <EffectCard></EffectCard>;
+    });
+  };
+  renderEventCard = () => {
+    const arr = [1, 2, 3, 4];
     return arr.map(({ index }) => {
       return <EffectCard></EffectCard>;
     });
@@ -18,14 +44,38 @@ class LateGlobalWarming extends React.Component {
       <div class="WarmingContainer">
         <div class="WarmingLeft"></div>
         <div class="WarmingMid">
+          <div class="SelectDailyEvent">
+            <div
+              class="Daily"
+              onClick={this.handleClick}
+              style={
+                this.state.daily === true
+                  ? { color: "red" }
+                  : { color: "black" }
+              }
+            >
+              Daily
+            </div>
+            <div
+              class="Event"
+              onClick={this.handleClick}
+              style={
+                this.state.daily === false
+                  ? { color: "red" }
+                  : { color: "black" }
+              }
+            >
+              Event
+            </div>
+          </div>
           <div class="WarmingTextBox1">
-            <h3>현재 지구의 표면 온도</h3>
-            <text>24.01도</text>
+            <h3>일상 생활에서는 무엇을 할 수 있나요?</h3>
           </div>
-          <div class="WarmingTextBox2">
-            <text class="WarmingText">0.01도 증가하면</text>
+          <div class="cardsBox">
+            {this.state.daily === true
+              ? this.renderDailyCard()
+              : this.renderEventCard()}
           </div>
-          <div class="cardsBox">{this.renderCard()}</div>
           <div class="WarmingCards"></div>
         </div>
         <div class="WarmingRight"></div>
